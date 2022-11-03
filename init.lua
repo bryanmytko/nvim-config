11bb94473e67fd8eda867a170e09c06bd0763e89
Bryan Mytko <bryanmytko@gmail.com>
2022-11-03 02:36:12 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+require("bryan.plugins-setup")
+
+require("bryan.core.options")
+require("bryan.core.keymaps")
+require("bryan.core.colorscheme")
+
+require("bryan.plugins.comment")
+require("bryan.plugins.lualine")
+require("bryan.plugins.telescope")
+require("bryan.plugins.nvim-cmp")
+
+require("bryan.plugins.lsp.mason")
+require("bryan.plugins.lsp.lspsaga")
+require("bryan.plugins.lsp.lspconfig")
+require("bryan.plugins.lsp.null-ls")
