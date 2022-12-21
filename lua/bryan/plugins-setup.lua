f72607958d462dbbdc7fd9cf9984bed1ea5cf0f9
Bryan Mytko <bryanmytko@gmail.com>
2022-12-21 15:41:46 -0800

    Add js syntax highlighting

--- lua/bryan/plugins-setup.lua
+++ lua/bryan/plugins-setup.lua
@@ -39,6 +39,9 @@ return packer.startup(function(use)
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
+	-- syntax highlighting
+	use("pangloss/vim-javascript")
+
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("hrsh7th/cmp-buffer") -- source for text in buffer
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -211,6 +211,11 @@ _G.packer_plugins = {
     path = "/Users/bryan/.local/share/nvim/site/pack/packer/start/typescript.nvim",
     url = "https://github.com/jose-elias-alvarez/typescript.nvim"
   },
+  ["vim-javascript"] = {
+    loaded = true,
+    path = "/Users/bryan/.local/share/nvim/site/pack/packer/start/vim-javascript",
+    url = "https://github.com/pangloss/vim-javascript"
+  },
   ["vim-nightfly-guicolors"] = {
     loaded = true,
     path = "/Users/bryan/.local/share/nvim/site/pack/packer/start/vim-nightfly-guicolors",
