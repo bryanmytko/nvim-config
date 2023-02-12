30e5396eb2e80cd2e7c95d46892b1f6090741558
Bryan Mytko <bryanmytko@gmail.com>
2023-02-12 15:02:35 -0800

    Add rust_analyzer

--- lua/bryan/plugins/lsp/mason.lua
+++ lua/bryan/plugins/lsp/mason.lua
@@ -13,7 +13,7 @@ end
 -- import mason-null-ls plugin safely
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  print("no mason null ls")
+	print("no mason null ls")
 	return
 end
 
@@ -23,11 +23,12 @@ mason.setup()
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"tsserver",
-		"html",
 		"cssls",
-		"tailwindcss",
+		"html",
 		"sumneko_lua",
+		"tailwindcss",
+		"tsserver",
+		"rust_analyzer",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
