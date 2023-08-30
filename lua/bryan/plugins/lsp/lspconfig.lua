57898e772ebde9aaf8dfc4194c034079ade1327e
Bryan Mytko <bryanmytko@gmail.com>
2023-08-30 00:41:15 -0400

    Turn back on Rust; fix config, remove double comments

--- lua/bryan/plugins/lsp/lspconfig.lua
+++ lua/bryan/plugins/lsp/lspconfig.lua
@@ -11,19 +11,22 @@ if not cmp_nvim_lsp_status then
 	return
 end
 
--- -- import typescript plugin safely
--- local typescript_setup, typescript = pcall(require, "tsserver")
--- if not typescript_setup then
--- 	print("ctss sever bok")
+-- import typescript plugin safely
+local typescript_setup, typescript = pcall(require, "typescript")
+if not typescript_setup then
+	print("ctss sever bok")
+	return
+end
 
--- 	return
--- end
+if not typescript then
+	print("typescript not loaded")
+	return
+end
 
--- -- import tailwindcss plugin safely
+-- import tailwindcss plugin safely
 -- local tailwindcss_setup, tailwindcss = pcall(require, "tailwindcss")
 -- if not tailwindcss_setup then
 -- 	print("TSS not found")
-
 -- 	return
 -- end
 
@@ -33,9 +36,8 @@ local keymap = vim.keymap -- for conciseness
 local on_attach = function(client, bufnr)
 	-- keybind options
 	local opts = { noremap = true, silent = true, buffer = bufnr }
-
 	-- set keybinds
-	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	keymap.set("n", "gf", "<cmd>Lfpsaga lsp_finder<CR>", opts) -- show definition, references
 	keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
 	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
 	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
@@ -47,7 +49,6 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
 	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
 	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
-
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
 		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
@@ -73,6 +74,14 @@ lspconfig["html"].setup({
 	on_attach = on_attach,
 })
 
+-- configure typescript
+typescript.setup({
+	server = {
+		capabilities = capabilities,
+		on_attach = on_attach,
+	},
+})
+
 -- configure css server
 lspconfig["cssls"].setup({
 	capabilities = capabilities,
@@ -90,12 +99,12 @@ require("lspconfig")["rust_analyzer"].setup({
 	},
 })
 
-require("lspconfig")["tsserver"].setup({
-	server = {
-		capabilities = capabilities,
-		on_attach = on_attach,
-	},
-})
+-- require("lspconfig")["tsserver"].setup({
+-- 	server = {
+-- 		capabilities = capabilities,
+-- 		on_attach = on_attach,
+-- 	},
+-- })
 
 -- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
